272b719a651a03e192a4a7ded2057abd2c27bbf2
Karolina Karkosiñska <erie@wp.pl>
2021-12-31 16:17:54 +0100

    MainExtendedRepository

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main (String[] args) {
+        String country = "Poland";
+        String capital = "Warsaw";
+        int population = 38268000;
+        double areaSqM = 312.696;
+        char firstCountryLetter = 'P';
+        boolean AnthemPolandIsNotYetLost = true;
+        System.out.println(country);
+        System.out.println(capital);
+        System.out.println(population);
+        System.out.println(areaSqM);
+        System.out.println(firstCountryLetter);
+        System.out.println(AnthemPolandIsNotYetLost);
+    }
+}
