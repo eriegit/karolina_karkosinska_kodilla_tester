926865f8c348980c3043decb196c56df5336f75c
Karolina Karkosiñska <erie@wp.pl>
2022-01-18 18:05:37 +0100

    2.5. Ćwiczenia: pętle - "RandomNumbers"

---
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class RandomNumbers {
+    public static int min = 31;
+    public static int max = 0;
+
+
+    public static void main(String[] args) {
+        getCountOfRandomNumber(5001);
+        System.out.println("min " + min + ", max " + max);
+    }
+
+    public static void getCountOfRandomNumber(int limit) {
+        Random random = new Random();
+        int sum = 0;
+        while (sum <= limit) {
+            int temp = random.nextInt(31);
+            sum = sum + temp;
+            // System.out.println("temp " + temp + ", sum " + sum);
+            if (temp > max) {
+                max = temp;
+            }
+            if (temp < min) {
+                min = temp;
+            }
+        }
+    }
+}
+
