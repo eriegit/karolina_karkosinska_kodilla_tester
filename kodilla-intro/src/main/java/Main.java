616ee6efae8eb326b889799256bf9b069243a69a
Karolina Karkosiñska <erie@wp.pl>
2022-02-06 16:09:00 +0100

    Main again

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String[] args) {
+        String myName = "Karolina";
+        int myAge = 34;
+        double height = 1.70;
+        char carClass = 'A' ;
+        boolean isaGoodProgrammer = true ;
+        System.out.println(myName);
+        String task = "Hello from FirstClass!";
+        System.out.println(task);
+    }
+}
